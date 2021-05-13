bcc302a743edf074518cab48afcedbbeafe18d5a
Irving Mondragón <mirvingr@gmail.com>
2021-05-13 12:56:52 -0500

    Update name of Docker repository

--- cicd/scripts/deploy-stage.sh
+++ cicd/scripts/deploy-stage.sh
@@ -6,4 +6,4 @@ echo "Stage 4. Deploy"
 echo $REGISTRY_PASSWORD | docker login --username $REGISTRY_USERNAME --password-stdin
 docker stop webapp || true 
 docker rm webapp || true
-docker run -d --name webapp -p 8080:80 irvingmg/test:${BUILD_ID}
+docker run -d --name webapp -p 8080:80 irvingmg/web-app:${BUILD_ID}
